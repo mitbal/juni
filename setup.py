9cb40b38dce0c70eeb61723f1ea2e57eced7e940
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-07-11 14:30:42 +0700

    Updated the setup, freezing the dependency version

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="july",
-    version="0.1.3",
-    author="Edvard Hultén",
-    author_email="edvard.hulten@gmail.com",
+    name="juni",
+    version="0.0.1",
+    author="M Iqbal Tawakal",
+    author_email="mit.iqi@gmail.com",
     description="A small library for creating pretty heatmaps of daily data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
     license="MIT",
-    url="https://github.com/e-hulten/july/",
+    url="https://github.com/mitbal/juni/",
     keywords=[
         "heatmap",
         "visualisation",
@@ -35,8 +35,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=[
-        "matplotlib",
-        "numpy",
+        "matplotlib==3.9.0",
+        "numpy==1.26.4",
     ],
     python_requires=">=3.6",
 )
